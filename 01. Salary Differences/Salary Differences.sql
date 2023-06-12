5326c77486f12dd3992dbbfb9c69de38e0b38d3a
kohjingyan <kohjingyan@gmail.com>
2023-06-12 18:53:18 +0800

    Update Salary Differences.sql

--- 01. Salary Differences/Salary Differences.sql
+++ 01. Salary Differences/Salary Differences.sql
@@ -1,8 +1,13 @@
-with db_highest_salary AS (
-SELECT d.department, MAX(salary) AS highest_salary
-FROM db_employee e INNER JOIN
-db_dept d ON e.department_id = d.id
+WITH db_highest_salary AS (
+SELECT 
+  d.department, 
+  MAX(salary) AS highest_salary
+FROM db_employee e 
+INNER JOIN db_dept d 
+  ON e.department_id = d.id
 WHERE d.department = 'marketing' or d.department = 'engineering'
 GROUP BY d.department)
-SELECT MAX(highest_salary) - MIN(highest_salary)
+  
+SELECT 
+  MAX(highest_salary) - MIN(highest_salary)
 FROM db_highest_salary;
