5525151aa5d5013746d262af766cc25e5db3f470
kohjingyan <kohjingyan@gmail.com>
2023-06-12 23:12:16 +0800

    Update Users By Average Session Time.sql

--- 02. Users By Average Session Time/Users By Average Session Time.sql
+++ 02. Users By Average Session Time/Users By Average Session Time.sql
@@ -1,19 +1,20 @@
 WITH cte AS (
-	SELECT *, ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY timestamp) rn
+	SELECT *, 
+		ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY timestamp) rn
 	FROM facebook_web_log
 	WHERE action = 'page_load' OR action = 'page_exit')
-	SELECT x.user_id, AVG(x.t2 - x.t1)
-	FROM (
-		SELECT c1.user_id,
+SELECT x.user_id, AVG(x.t2 - x.t1)
+FROM (
+	SELECT c1.user_id,
 		c1.timestamp AS t1, 
 		c2.timestamp AS t2,
 		ROW_NUMBER() OVER (PARTITION BY c2.user_id, c2.rn ORDER BY (c2.user_id, c2.rn) ASC, c1.rn DESC) rn2
-		FROM cte c1
-		INNER JOIN cte c2
-		ON c1.action <> c2.action 
-			AND c1.user_id = c2.user_id
-			AND c1.timestamp < c2.timestamp
-		WHERE c1.action = 'page_load' 
-			AND c2.action = 'page_exit') x
-	WHERE x.rn2 = 1
-	GROUP BY x.user_id;
+	FROM cte c1
+	INNER JOIN cte c2
+	ON c1.action <> c2.action 
+		AND c1.user_id = c2.user_id
+		AND c1.timestamp < c2.timestamp
+	WHERE c1.action = 'page_load' 
+		AND c2.action = 'page_exit') x
+WHERE x.rn2 = 1
+GROUP BY x.user_id;
