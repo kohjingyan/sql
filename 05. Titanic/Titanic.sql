5a0a61b70b784e0177701e3969f008ac21f2c1bf
kohjingyan <kohjingyan@gmail.com>
2023-06-23 10:23:34 +0800

    Update Titanic.sql

--- 05. Titanic/Titanic.sql
+++ 05. Titanic/Titanic.sql
@@ -1,15 +1,15 @@
 -- Answer 1
-select pclass, 
-SUM(CASE WHEN survived = 0 THEN 1 ELSE 0 END) AS non_survivors,
-SUM(CASE WHEN survived = 1 THEN 1 ELSE 0 END) AS survivors
-from titanic
-group by pclass
+SELECT pclass, 
+  SUM(CASE WHEN survived = 0 THEN 1 ELSE 0 END) AS non_survivors,
+  SUM(CASE WHEN survived = 1 THEN 1 ELSE 0 END) AS survivors
+FROM titanic
+GROUP BY pclass
 ORDER BY pclass;
 
 -- Answer 2
 SELECT survived,
-SUM(CASE WHEN pclass = 1 THEN 1 ELSE 0 END) AS first_class,
-SUM(CASE WHEN pclass = 2 THEN 1 ELSE 0 END) AS second_class,
-SUM(CASE WHEN pclass = 3 THEN 1 ELSE 0 END) AS third_class
+  SUM(CASE WHEN pclass = 1 THEN 1 ELSE 0 END) AS first_class,
+  SUM(CASE WHEN pclass = 2 THEN 1 ELSE 0 END) AS second_class,
+  SUM(CASE WHEN pclass = 3 THEN 1 ELSE 0 END) AS third_class
 FROM titanic
 GROUP BY survived;
