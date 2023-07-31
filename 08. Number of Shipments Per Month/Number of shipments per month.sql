ee5dc81ef2c747d882cd7476e33d73ff8d6047b5
kohjingyan <kohjingyan@gmail.com>
2023-07-31 10:43:47 +0800

    Add files via upload

---
+++ 08. Number of Shipments Per Month/Number of shipments per month.sql
@@ -0,0 +1,8 @@
+WITH cte AS(
+	SELECT shipment_id, 
+	       DATE_FORMAT(shipment_date, '%Y-%m') AS date_ym
+	FROM amazon_shipment)
+SELECT date_ym, 
+       COUNT(shipment_id) AS no_of_shipments
+FROM cte
+GROUP BY date_ym;
